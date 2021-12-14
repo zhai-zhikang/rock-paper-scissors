736319ec49d01b46b8dd58838a9ad690562aaf4f
zkzhai <3162855960@qq.com>
2021-12-14 17:06:05 +0800

    this is init model for 剪刀石头布识别

--- cnn_model.py
+++ cnn_model.py
@@ -1,1 +1,50 @@
-# test
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from matplotlib import image
+from tqdm import tqdm
+import os
+import numpy as np
+import cv2
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# temp = []
+# files = os.listdir('./rock-paper-scissors/train/rock')
+# for file in files:
+#     image = cv2.imread('./rock-paper-scissors/train/rock/'+file)
+#     print(image.shape)
+#     temp.append(image.shape)
+train = ImageDataGenerator(rescale=1 / 255)
+test = ImageDataGenerator(rescale=1 / 255)
+train_generator = train.flow_from_directory(
+    './rock-paper-scissors/train',
+    target_size=(500,500),
+    batch_size=50,
+    class_mode='sparse'
+)
+test_generator = test.flow_from_directory(
+    './rock-paper-scissors/test',
+    target_size=(500,500),
+    batch_size=50,
+    class_mode='sparse'
+)
+
+# 搭建模型
+model = keras.Sequential()
+# 两层卷积
+model.add(keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu, input_shape=(500,500,3)))
+model.add(keras.layers.MaxPool2D(2, 2))
+model.add(keras.layers.Conv2D(64, (3, 3), activation=tf.nn.relu))
+model.add(keras.layers.MaxPool2D(2, 2))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(keras.layers.Dense(3, activation=tf.nn.softmax))
+# 训练与测试
+model.compile(optimizer=tf.optimizers.Adam(), loss=tf.losses.sparse_categorical_crossentropy,
+              metrics=['accuracy'])
+history = model.fit(
+    train_generator,
+    epochs=10,
+    validation_data=test_generator
+)
+model.save('model_v1.h5')
--- main.py
+++ main.py
@@ -1,2 +1,43 @@
 # This is a Rock-paper-scissors dynamic identification code
-print('hhh')
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import cv2
+import os
+import numpy as np
+
+model = load_model('model_v1.h5')
+# 读取视频
+vc = cv2.VideoCapture(0)
+if vc.isOpened():
+    open, frame = vc.read()
+else:
+    open = False
+class_indices = ['paper','rock','scissors']
+while open:
+    ret, frame = vc.read()
+    if frame is None:
+        break
+    if ret:
+        #预测
+        pic = cv2.resize(frame[220:420,140:340,:], (500,500))
+        name = class_indices[np.argmax(model.predict(pic.reshape(1, 500, 500, 3)))]
+        #加工图片
+        #边缘检测
+        sobelx = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=3)
+        sobelx = cv2.convertScaleAbs(sobelx)
+        sobely = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=3)
+        sobely = cv2.convertScaleAbs(sobely)
+        frame = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
+        res = cv2.rectangle(frame,(220,140),(420,340),color=(0, 100, 0),thickness=2)
+        res = cv2.putText(res, name, (220, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 200))
+        cv2.imshow('rock-paper-scissors', res)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+vc.release()
+cv2.destroyAllWindows()
+
+# files = os.listdir('./rock-paper-scissors/train/rock')
+# image = cv2.imread('./rock-paper-scissors/train/rock/'+files[0])
+# pic = cv2.resize(image, (500, 500))
+# model.predict(pic.reshape(1, 500, 500, 3))
+
